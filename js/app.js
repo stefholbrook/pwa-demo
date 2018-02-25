c311d5aef2b18fb2e1b0162ad2c2c7d659481a39
Stefanie Holbrook <stefholbrook@gmail.com>
2018-02-25 12:14:37 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -1,46 +1,50 @@
-(function () {
+(function() {
   Date.prototype.yyyymmdd = function() {
-    // getMonth is zero based,
-    // so we increment by 1
-    let mm = this.getMonth() + 1
-    let dd = this.getDate()
+    let mm = this.getMonth() + 1;
+    let dd = this.getDate();
 
     return [this.getFullYear(),
             (mm>9 ? '' : '0') + mm,
             (dd>9 ? '' : '0') + dd
-          ].join('-')
-  }
+          ].join('-');
+  };
+
+  // const data = {
+  //   full_name: 'owner/repository',
+  //   language: 'JavaScript',
+  //   stargazers_count: 250,
+  //   forks: 19,
+  //   description: 'What the project is about',
+  //   html_url: ''
+  // };
 
   const dates = {
     startDate: function() {
-       const startDate = new Date()
-       startDate.setDate(startDate.getDate() - 7)
-       return startDate.yyyymmdd()
-     },
-     endDate: function() {
-       const endDate = new Date()
-       return endDate.yyyymmdd()
-     }
-   }
-
-  const app = {
-    apiURL: `https://api.github.com/search/repositories?q=created:%22${dates.startDate()}+..+${dates.endDate()}%22%20language:javascript&sort=stars&order=desc`,
-    cardTemplate: document.querySelector('.card-template')
-  }
-
-  app.updateTrends = (trends) => {
-    const trendsRow = document.querySelector('.trends')
-
-    for(let i = 0; i < trends.length; i++) {
-      const trend = trends[i]
-
-      trendsRow.appendChild(app.createCard(trend))
+      const startDate = new Date();
+      startDate.setDate(startDate.getDate() - 7);
+      return startDate.yyyymmdd();
+    },
+    endDate: function() {
+      const endDate = new Date();
+      return endDate.yyyymmdd();
     }
   }
 
-  app.createCard = (trend) => {
-    const card = app.cardTemplate.cloneNode(true)
+  const app = {
+    apiURL: `https://api.github.com/search/repositories?q=created%3A%22${dates.startDate()}+..+${dates.endDate()}%22%20language%3Ajavascript&sort=stars&order=desc`,
+    cardTemplate: document.querySelector('.card-template')
+  }
 
+  app.updateTrends = function(trends) {
+    const trendsRow = document.querySelector('.trends');
+    for(let i = 0; i < trends.length; i++) {
+      const trend = trends[i];
+      trendsRow.appendChild(app.createCard(trend));
+    }
+  }
+
+  app.createCard = function(trend) {
+    const card = app.cardTemplate.cloneNode(true);
     card.classList.remove('card-template')
     card.querySelector('.card-title').textContent = trend.full_name
     card.querySelector('.card-lang').textContent = trend.language
@@ -48,24 +52,22 @@
     card.querySelector('.card-forks').textContent = trend.forks
     card.querySelector('.card-link').setAttribute('href', trend.html_url)
     card.querySelector('.card-link').setAttribute('target', '_blank')
-
-    return card
+    return card;
   }
 
-  app.getTrends = function () {
-    const networkReturned = false
-
+  app.getTrends = function() {
+    const networkReturned = false;
     if ('caches' in window) {
       caches.match(app.apiURL).then(function(response) {
         if (response) {
           response.json().then(function(trends) {
             console.log('From cache...')
             if(!networkReturned) {
-              app.updateTrends(trends.items)
+              app.updateTrends(trends.items);
             }
-          })
+          });
         }
-      })
+      });
     }
 
     fetch(app.apiURL)
@@ -79,18 +81,17 @@
     });
   }
 
-  document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', function() {
     app.getTrends()
-
-    // Event listener for refresh button
-    const refreshButton = document.querySelector('.refresh')
+    const refreshButton = document.querySelector('.refresh');
     refreshButton.addEventListener('click', app.getTrends)
   })
 
-  // Check if the browser supports service workers
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('service-worker.js')
-      .then(() => console.log('ServiceWorker registered'))
+     .register('service-worker.js')
+     .then(function() {
+        console.log('Service Worker Registered');
+      });
   }
 })()
